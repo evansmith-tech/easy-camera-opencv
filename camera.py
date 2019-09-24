d56f68001343c22ae5b151e500183d0c6f98de24
Evan Smith <evan.c.smith@ttu.edu>
2019-09-23 20:55:05 -0500

    Initial Push

---
+++ camera.py
@@ -0,0 +1,39 @@
+import cv2
+import time
+import os
+
+class Camera:
+    vid_capture = cv2.VideoCapture(0)
+    vid_cod = cv2.VideoWriter_fourcc(*'XVID')
+    outputFilePath = './'
+    def __init__(self, _outputFilePath):
+        self.outputFilePath = os.path.dirname(_outputFilePath) # takes our string and converts it to a file path
+        # If the directory already exists, we don't create it 
+        # but we can still access it later on obviously
+        # stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
+        os.makedirs(self.outputFilePath, exist_ok=True) # Creates the nested directories needed by the user
+                
+                        
+    def record(self, _secondsToRecord, _filename):
+        filePath = self.outputFilePath + "/" + _filename + ".mp4"
+        output = cv2.VideoWriter(filePath, self.vid_cod, 30.0, (640 , 480))
+
+        endTime = time.time() + _secondsToRecord
+        while(time.time() <= endTime):
+            # Capture each frame of webcam video
+            ret,frame = self.vid_capture.read()
+            output.write(frame)
+
+        # close the already opened camera
+        self.vid_capture.release()
+        # close the already opened file
+        output.release()
+        
+
+    def disconnectCamera(self):
+        # Disconects camera
+        self.vid_capture.release()
+
+cam = Camera("./go/to/there/")
+cam.record(5, 'swag')
+cam.disconnectCamera()
